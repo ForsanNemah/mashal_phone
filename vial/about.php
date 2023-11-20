0c5e4b7442e8c3c45e491c179381a1642274e531
ForsanNemah <forsan20172017@gmail.com>
2023-11-21 00:45:11 +0300

    added

---
+++ vial/about.php
@@ -0,0 +1,24 @@
+
+ 
+
+
+
+<p class="text-right " style="background: #D3D3D3;" dir="rtl">
+    
+    
+
+دكتور/كريم عسران
+<br>
+<br>
+خبير  تجميل وزراعة الاسنان 
+
+<br>
+<br>
+ 
+
+
+</p>
+
+
+
+
---
+++ vial/footer.php
@@ -0,0 +1,26 @@
+
+    <!-- Footer -->
+    <footer class="page-footer font-small blue">
+    
+<!-- Copyright -->
+ 
+
+<div class="footer-copyright text-center py-3">    
+   
+ 
+ 
+
+   
+</div>
+<!-- Copyright -->
+
+
+<div class="footer-copyright text-center py-3">    
+ 
+ 
+ <a style="color:blue;" href="privicy.php">سياسة الخصوصية  </a>
+
+   
+</div>
+
+</footer>
